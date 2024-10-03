d5859d85bbff1a91a67395d95aacf3ffe296abcb
taeyang0206 <taengsun0206@gmail.com>
2024-10-03 16:39:12 +0900

    Comment / Post entity

--- src/main/java/com/kwonkim/blog/comment/Comment.java
+++ src/main/java/com/kwonkim/blog/comment/Comment.java
@@ -1,4 +1,38 @@
 package com.kwonkim.blog.comment;
 
+import com.kwonkim.blog.post.Post;
+import com.kwonkim.blog.user.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@NoArgsConstructor
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 기본으로 할당해주는 어노테이션
+    private Long id;
+
+    @Column(name = "uuid", columnDefinition = "BINARY(16)", nullable = false, unique = true)
+    private UUID comment_uuid;
+
+    @ManyToOne(fetch = FetchType.LAZY) // FetchType을 lazy로 두면 실제 객체를 사용하는 시점에 쿼리를 날린다.
+    @JoinColumn(name = "post_id", nullable = false)
+    private List<Post> post = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "isDelete", nullable = false)
+    protected boolean isDelete;
+
 }
--- src/main/java/com/kwonkim/blog/post/Post.java
+++ src/main/java/com/kwonkim/blog/post/Post.java
@@ -1,4 +1,42 @@
 package com.kwonkim.blog.post;
 
+import com.kwonkim.blog.comment.Comment;
+import com.kwonkim.blog.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "post")
+@Getter
+@NoArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 기본으로 할당해주는 어노테이션
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(name = "uuid", columnDefinition = "BINARY(16)", nullable = false, unique = true)
+    private UUID post_uuid;
+
+    @ManyToOne(fetch = FetchType.LAZY) // FetchType을 lazy로 두면 실제 객체를 사용하는 시점에 쿼리를 날린다.
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "post_title", nullable = false)
+    private String title;
+
+    @Column(name = "post_content", nullable = false)
+    private String content;
+
+    @OneToMany(mappedBy = "comment")
+    private List<Comment> comment = new ArrayList<>();
+
+    @Column(name = "isDelete", nullable = false)
+    protected boolean isDelete;
+
 }
