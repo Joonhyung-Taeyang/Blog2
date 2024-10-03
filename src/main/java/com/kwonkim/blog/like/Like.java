54f423c7ef9803059d2aeaa13b8a52ad41457f9d
kwonjh0406 <kwonjh0406@gmail.com>
2024-10-03 16:19:56 +0900

    user, like Entity 생성

--- src/main/java/com/kwonkim/blog/like/Like.java
+++ src/main/java/com/kwonkim/blog/like/Like.java
@@ -1,4 +1,27 @@
 package com.kwonkim.blog.like;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private int user_id;
+
+    @Column(nullable = false)
+    private int post_id;
+
+    @Column(nullable = false)
+    private boolean isDelete;
+
 }
--- src/main/java/com/kwonkim/blog/user/User.java
+++ src/main/java/com/kwonkim/blog/user/User.java
@@ -1,4 +1,29 @@
 package com.kwonkim.blog.user;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
 }
