73aa0eace82a5eae94607f9431f5892f9e3dcd82
kwonjh0406 <kwonjh0406@gmail.com>
2024-10-03 18:00:12 +0900

    html 뼈대 생성

--- src/main/java/com/kwonkim/blog/user/UserRepository.java
+++ src/main/java/com/kwonkim/blog/user/UserRepository.java
@@ -1,4 +1,6 @@
 package com.kwonkim.blog.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/kwonkim/blog/user/UserService.java
+++ src/main/java/com/kwonkim/blog/user/UserService.java
@@ -1,4 +1,17 @@
 package com.kwonkim.blog.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private int join(User user) {
+        userRepository.save(user);
+        return 1;
+    }
+
 }
