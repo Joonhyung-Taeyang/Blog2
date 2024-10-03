a3990db7c702c954b0576a8e500cfd96acd1348d
kwonjh0406 <kwonjh0406@gmail.com>
2024-10-03 16:30:56 +0900

    like entity 예약어 생성 제한 Like ->  LikeCheck 로 entity 이름 변경

--- src/main/java/com/kwonkim/blog/like/LikeCheck.java
+++ src/main/java/com/kwonkim/blog/like/LikeCheck.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Like {
+public class LikeCheck {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
